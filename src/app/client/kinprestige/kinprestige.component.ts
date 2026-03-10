faaef58c30e7c0c0a10cd2b4f6410793a43d70bf
Michée Kolony <micheekolony71@gmail.com>
2026-03-10 14:32:36 +0100

    ajout spinner de chargement

--- src/app/client/kinprestige/kinprestige.component.ts
+++ src/app/client/kinprestige/kinprestige.component.ts
@@ -7,17 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KinprestigeComponent implements OnInit {
 
-
+  loading: boolean = true; // Ajout de la propriété loading
   showCookieModal = false;
 
   ngOnInit() {
-  
-    const consent = localStorage.getItem('cookieConsent');
-  
-    if (!consent) {
-      this.showCookieModal = true;
+    // Simuler le chargement de la page
+    setTimeout(() => {
+      this.loading = false; // Cache le spinner après le chargement
+      
+      // Gestion du cookie modal après le chargement
+      const consent = localStorage.getItem('cookieConsent');
+      if (!consent) {
+        this.showCookieModal = true;
+      }
+    }, 1000); // Ajustez le délai selon vos besoins
+    
+    // Alternative : utiliser les événements de chargement du navigateur
+    if (document.readyState === 'complete') {
+      this.loading = false;
+    } else {
+      window.addEventListener('load', () => {
+        this.loading = false;
+      });
     }
-  
   }
   
   acceptCookies() {
